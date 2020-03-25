06ea7a9792c80398aea72eb8808c93c27282acaa
RafaelM <RafaelM.Science@gmail.com>
2020-03-25 16:43:40 -0400

    Feat: back end

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 
 import OngsController from './controllers/OngsController';
+import IncidentController from './controllers/IncidentController';
+import ProfileController from './controllers/ProfileController';
+import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.create);
+
 routes.get('/ongs', OngsController.index);
+routes.get('/incidents', IncidentController.index);
+routes.get('/profile', ProfileController.index);
 
 routes.post('/ongs', OngsController.create);
+routes.post('/incidents', IncidentController.create);
+
+routes.delete('/incidents/:id', IncidentController.delete);
 
 export default routes;
